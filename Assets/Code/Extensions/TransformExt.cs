8b08b6beb2378dd41bdca85d9a82f27adc63221a
ThomasMcSherry <54848048+ThomasMcSherry@users.noreply.github.com>
2022-12-26 21:44:58 +0000

    Added assets/tools
    Added:
    2dxFX(2D shaders)
    DAX Particle Effects
    ProCamera2D
    Odin Inspector
    TextFX
    Better UI
    Extension method scripts

---
+++ Assets/Code/Extensions/TransformExt.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExt{
+	
+	public static void CopyTo(this Transform source, Transform to){
+		to.position = source.position.Clone();
+		to.localEulerAngles = source.localEulerAngles.Clone();
+		to.localPosition = source.localPosition.Clone();
+		to.localRotation = source.localRotation.Clone();
+		to.localScale = source.localScale.Clone();
+		to.rotation = source.rotation.Clone();
+	}
+	
+	public static bool HasParent(this Transform t){
+		return t.parent != null;
+	}
+	
+	public static List<GameObject> GetAllChildren(this Transform t){
+		return t.gameObject.GetAllChildren();
+	}
+
+	public static T GetClosestParentComponent<T>(this Transform transform) where T : Component {
+		if (!transform.HasParent()) {
+			return default(T);
+		}
+		Transform activeObject = transform;
+
+		T t = activeObject.GetComponentInParent<T>();
+
+		if(t == null) {
+			return activeObject.parent.GetClosestParentComponent<T>();
+        } else {
+			return t;
+        }
+
+	}
+
+	public static T GetParentComponent<T>(this Transform mb) where T : Component{
+		if(!mb.HasParent()){
+			return default(T);
+		}
+		Transform parent = mb.parent;
+		T t = parent.gameObject.GetComponent<T>();
+		return t;
+	}
+
+	public static void BringToFront(this Transform t) {
+		t.SetAsLastSibling();
+    }
+}
+		
