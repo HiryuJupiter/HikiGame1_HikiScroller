be6d38752b0327c95a23868c6e9c11c2490d6f42
ThomasMcSherry <54848048+ThomasMcSherry@users.noreply.github.com>
2022-12-26 23:01:34 +0000

    Dialogue System + Combat
    Started on the combat backend
    Added Dialogue System asset

---
+++ Assets/Code/Frozen Acorn/Combat/Stats.cs
@@ -0,0 +1,42 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Stats : MonoBehaviour {
+
+    [SerializeField]
+    int strength = 3;
+    [SerializeField]
+    int maxHealth = 10;
+    int health = 10;
+
+    [ShowInInspector]
+    public int Health { get => health; }
+    public float HealthPercentage { get => (float)health / (float)maxHealth; }
+    [ShowInInspector]
+    public bool IsDead { get => health <= 0; }
+    public int Strength { get => strength; }
+
+    [SerializeField]
+    UnityEvent onDeath;
+
+    void Awake() {
+        health = maxHealth;
+    }
+    [Button]
+    public void TakeDamage(int damage) {
+        if(!IsDead) {
+            health -= damage;
+            if (IsDead) {
+                onDeath.Invoke();
+            }
+        }
+    }
+
+
+
+
+
+}
