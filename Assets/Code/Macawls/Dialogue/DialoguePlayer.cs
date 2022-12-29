91ce65385a7cfbd41ed958ebcdd79a149f155a49
Macawls <josh@macawls.dev>
2022-12-30 00:00:34 +0200

    add dialogue player

---
+++ Assets/Code/Macawls/Dialogue/DialoguePlayer.cs
@@ -0,0 +1,33 @@
+﻿using CleverCrow.Fluid.Dialogues.Graphs;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Dialogue
+{
+    public class DialoguePlayer : SerializedMonoBehaviour
+    {
+        [SerializeField] private IDialoguePlayer dialoguePlayer;
+        [SerializeField] private DialogueGraph graphToPlay;
+
+        private bool IsPlayMode => Application.isPlaying;
+        
+        [ShowIf("IsPlayMode")]
+        [Button("Play Dialogue")]
+        public void Play()
+        {
+            if (dialoguePlayer is null)
+            {
+                Debug.LogError("No dialogue player set");
+                return;
+            }
+
+            if (!graphToPlay)
+            {
+                Debug.LogError("No dialogue graph set");
+                return;
+            }
+            
+            dialoguePlayer.PlayDialogue(graphToPlay);
+        }
+    }
+}
