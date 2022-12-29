86ebdda4da65f7b3d745f4223f877bcc18672b77
HiryuJupiter <69824436+HiryuJupiter@users.noreply.github.com>
2022-12-30 01:58:35 +1100

    Added an end scene with a scrolling text (press J to progress text)

---
+++ Assets/Code/Miyaru/UI/Ending/EndScrollTextManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EndScrollTextManager : MonoBehaviour
+{
+    public ScrollTextWindow textWindow;
+    public CanvasGroupFader blackScreenFader;
+    public string[] sentences;
+
+    int currentIndex = -1;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ReadNextSentence();
+    }
+
+    void Update()
+    {
+        if (canRead && Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            ReadNextSentence();
+        }
+    }
+
+    void ReadNextSentence()
+    {
+        currentIndex++;
+        string next = "";
+        if (currentIndex < sentences.Length)
+        {
+            next = sentences[currentIndex];
+            textWindow.Show(next);
+        }
+        else //Reached end
+        {
+            textWindow.Close();
+            blackScreenFader.FadeTo100();
+        }
+    }
+
+    bool canRead => textWindow != null && sentences.Length > 0 && currentIndex < sentences.Length;
+}
---
+++ Assets/Code/Miyaru/UI/Ending/ScrollTextWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class ScrollTextWindow : MonoBehaviour
+{
+    public TMP_Text Text;
+    string currentText;
+
+    CanvasGroup cvs;
+
+    void Start()
+    {
+        cvs = GetComponent<CanvasGroup>();
+        cvs.alpha = 0;
+    }
+
+    public void Show(string text)
+    {
+        cvs.alpha = 1;
+        currentText = text;
+        StartCoroutine(DoDisplayText());
+    }
+
+    public void Close ()
+    {
+        cvs.alpha = 0;
+        StopAllCoroutines();
+    }
+
+    IEnumerator DoDisplayText()
+    {
+        Text.text = "";
+
+        foreach (char c in currentText.ToCharArray())
+        {
+            Text.text += c;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
